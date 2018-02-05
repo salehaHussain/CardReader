3abad66c359eafcc57e3bcbfe064657faf5356a2
Saleha Hussain <salehahu@microsoft.com>
2018-02-05 22:29:26 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/salehahu/cardreader/DataExtractor.java
@@ -0,0 +1,13 @@
+package com.example.salehahu.cardreader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DataExtractor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_extractor);
+    }
+}
